0081da8df836b7ab3862f3f03c9e83f9ec420f9e
Setyo Wibowo ID <64427533+0xSetyoWibowo@users.noreply.github.com>
2025-09-26 17:39:06 +0700

    Update claude.php

--- claude.php
+++ claude.php
@@ -3,7 +3,7 @@
 session_start();
 
 // Kata sandi untuk akses, sangat disarankan untuk mengubahnya
-$password = 'secret_password_123';
+$password = 'ainyakerenbos';
 
 // Fungsi untuk membersihkan input dan mencegah serangan XSS
 function clean_input($data) {
